ae852abbf27e4ec946556baa21dbf7c0852fad7c
Erik Iverson <erik@sigmafield.org>
2014-06-25 15:01:18 -0500

    add index markdown file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // pilrdocs build file
 
-var MetalSmith = require('metalsmith');
+var MetalSmith = require('metalsmith'),
+    markdown   = requrie('metalsmith-markdown');
 
 Metalsmith(__dirname)
+    .use(markdown())
     .destination('./build')
     .build();
 
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+title: Home
+---
+
+This is the PiLR docs landing page. 
+
+## Topics
+
