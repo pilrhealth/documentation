30dc846f60842ae207891481349f8603a9bd1cf2
Erik Iverson <erik@sigmafield.org>
2014-06-25 16:27:57 -0500

    add header/footer partials

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // pilrdocs build file
 
-var Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown'),
-    templates  = require('metalsmith-templates');
+var Metalsmith  = require('metalsmith'),
+    markdown    = require('metalsmith-markdown'),
+    templates   = require('metalsmith-templates'),
+    Handlebars  = require('handlebars'),
+    fs          = require('fs'),
+    collections = require('metalsmith-collections'),
+    permalinks  = require('metalsmith-permalinks');
+
+Handlebars.registerPartial('header', 
+                           fs.readFileSync(__dirname +
+'/templates/partials/header.hbt').toString());
+
+Handlebars.registerPartial('footer', 
+                           fs.readFileSync(__dirname +
+'/templates/partials/footer.hbt').toString());
 
 Metalsmith(__dirname)
+    .use(collections({
+                      pages: {
+                              pattern: 'content/pages/*.md'
+                             }
+                     }))
     .use(markdown())
+    .use(permalinks({
+                     pattern: ':collection/:title'
+                    }))
     .use(templates('handlebars'))
     .destination('./build')
     .build();
-
-
-
---
+++ src/content/pages/about.md
@@ -0,0 +1,8 @@
+---
+title: About
+template: page.hbt
+---
+
+This is the PiLR Health documentation. PiLR is a platform that enables
+researchers to collect, manage, and analyze data data from mobile
+health studies.
