8bde49a58e65c6f5c8d0d8a0664e9c387788c2cb
Erik Iverson <erik@sigmafield.org>
2014-07-14 15:04:02 -0500

    integrate gulp with metalsmith

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,30 @@ var minify_css = require('gulp-minify-css');
 var livereload = require('gulp-livereload');
 var connect = require('gulp-connect');
 var watch  = require('gulp-watch');
+var debug =  require('gulp-debug');
+var assign = require('lodash.assign');
+var gulp_front_matter = require('gulp-front-matter');
+var gulpsmith = require('gulpsmith'),
+    markdown    = require('metalsmith-markdown'),
+    templates   = require('metalsmith-templates'),
+    Handlebars  = require('handlebars'),
+    fs          = require('fs'),
+    collections = require('metalsmith-collections'),
+    permalinks  = require('metalsmith-permalinks');
+
 
 var js_dir = './build/assets/javascripts';
 var css_dir = './build/assets/styles';
 var inst_dir = './build';
+
+Handlebars.registerPartial('header', 
+                           fs.readFileSync(__dirname +
+'/templates/partials/header.hbt').toString());
+
+Handlebars.registerPartial('footer', 
+                           fs.readFileSync(__dirname +
+'/templates/partials/footer.hbt').toString());
+
 
 gulp.task('clean', function () {  
     return gulp.src('build', {read: false})
@@ -19,7 +39,7 @@ gulp.task('clean', function () {
 
 // process bower_components
 gulp.task('js_bower_components', function() {
-    gulp.src(['client/bower_components/**/*.min.js']) 
+    gulp.src(['src/assets/**/*.min.js']) 
         .pipe(flatten())
         .pipe(concat('bower.js'))
         .pipe(gulp.dest(js_dir));
@@ -27,17 +47,32 @@ gulp.task('js_bower_components', function() {
 
 // process bower css
 gulp.task('css_bower_components', function() {
-  gulp.src('./client/bower_components/**/*.css')
+  gulp.src('./src/assets/**/*.css')
         .pipe(concat('bower.css'))
         .pipe(minify_css({keepBreaks:true}))
         .pipe(gulp.dest(css_dir));
 });
 
-// process index.html
-gulp.task('html_index', function() {
-    gulp.src('src/index.html')
-        .pipe(gulp.dest(inst_dir))
-        .on('error', gutil.log);
+// process markdown files using metalsmith
+gulp.task('metalsmith', function() {
+    gulp.src('src/**/*.md')
+        .pipe(gulp_front_matter()).on("data", function(file) {
+            assign(file, file.frontMatter); 
+            delete file.frontMatter;
+        })
+        .pipe(
+            gulpsmith()
+                .use(collections({
+                    pages: {
+                        pattern: 'content/pages/*.md'
+                    }
+                }))
+                .use(markdown())
+                .use(permalinks({
+                    pattern: ':collection/:title'
+                }))
+                .use(templates('handlebars')))
+        .pipe(gulp.dest(inst_dir));
 });
 
 
@@ -58,14 +93,15 @@ gulp.task('livereload', function() {
 gulp.task('watch', function() {
     gulp.watch('./src/assets/**/*.js', ['js_bower_components']);
     gulp.watch('./src/assets/**/*.css', ['css_bower_components']);
-    gulp.watch('./src/index.md', ['html_index']);
+    gulp.watch('./src/index.md', ['metalsmith']);
+    gulp.watch('./src/content/**/*.md', ['metalsmith']);
 });
 
 // default task, build
-gulp.task('default', ['html_index', 'js_bower_components', 'css_bower_components']);
+gulp.task('default', ['metalsmith', 'js_bower_components', 'css_bower_components']);
 
 // server task, test
-gulp.task('server', ['html_index', 'js_bower_components', 'css_bower_components',
+gulp.task('server', ['metalsmith', 'js_bower_components', 'css_bower_components',
                      'watch', 'webserver', 'livereload']);
 
 
